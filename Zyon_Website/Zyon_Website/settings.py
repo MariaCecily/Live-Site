f61445e316d43cb06bcde3546ec528cc3fafe34c
Maria Cecily <t.mariacecily@gmail.com>
2025-05-25 13:12:26 +0530

    Configured DB & Django is live

--- Zyon_Website/Zyon_Website/settings.py
+++ Zyon_Website/Zyon_Website/settings.py
@@ -73,11 +73,16 @@ WSGI_APPLICATION = 'Zyon_Website.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+		'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zyon_packers_db',
+        'HOST': '127.0.0.1',
+        'USER':'root',
+        'PASSWORD':'root',
+        'PORT':'3306',
+        
+					}
+					}
 
 
 # Password validation
